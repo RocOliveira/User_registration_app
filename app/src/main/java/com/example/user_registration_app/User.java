74db79941c10f5693cdbf953ab49373903115214
Rodrigo <rocoliveira@hotmail.com>
2026-04-02 21:39:16 -0300

    Criando o DAO

--- app/src/main/java/com/example/user_registration_app/User.java
+++ app/src/main/java/com/example/user_registration_app/User.java
@@ -17,6 +17,36 @@ public class User {
     private String email; // email do usuário
     private String phone; // telefone do usuário
 
+//Criando o construtor da classe que será usado para criar um novo objeto User
 
+    public User(String name, String cpf, String email, String phone){
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    //Metodos getter e satter para acessar e modificar os dados do objeto User
+
+    //Retorna o ID do usuário
+    public int getId() {return id;}
+
+    //Define o ID do usuário(usuario pelo Room para preencher automaticamdente)
+    public void setId(int id) {this.id = id;}
+
+    //Retorna o nome do usuario
+    public String getName() {return name;}
+
+    //Retorna o cpf do usuario
+    public String getCpf() {return cpf;}
+
+    //Retorna o email do usuario
+    public String getEmail() {return email;}
+
+    //Retorna o phone do usuario
+    public String getPhone() {return phone;}
+
+
+}
 
 }
--- app/src/main/java/com/example/user_registration_app/UserDao.java
+++ app/src/main/java/com/example/user_registration_app/UserDao.java
@@ -1,4 +1,25 @@
 package com.example.user_registration_app;
 
+
+//O Data Access Object é o componente que serve para persistencia de dados. Serve para definir as
+// operações que podem ser feitas no banco de dados com relação a entidade User.
+
+//Importa as notações do Room necessarias para definiro DAO (Data Access Object)
+import  androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+//Importa a classe List do Java para retornar uma lista de usuários
+import java.util.List;
+
+@Dao
 public interface UserDao {
-}
+    //Metodo para inserir um uisuario na tabela do  Banco de Dados
+    //A anotação @Insert informa à Room que este método deve ser usado para inserir dado
+
+    @Insert
+    void insert(User user);
+
+
+
+    }
